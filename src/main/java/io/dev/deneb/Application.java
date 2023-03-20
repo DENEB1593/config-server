7d578f6828eaeaf4c3bf6d1abed2bbc503411f1b
deneb1593 <deneb1593@gmail.com>
2023-03-21 01:13:00 +0900

    dev properties

--- src/main/java/io/dev/deneb/Application.java
+++ src/main/java/io/dev/deneb/Application.java
@@ -2,13 +2,14 @@ package io.dev.deneb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@
-public class CloudServerConfigDemoApplication {
+@EnableConfigServer
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(CloudServerConfigDemoApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
