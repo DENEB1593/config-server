ac5e7765c3c5972196c686bd45e77a1239cc0a2a
deneb1593 <deneb1593@gmail.com>
2023-03-21 01:06:45 +0900

    first commit

---
+++ src/main/java/io/dev/deneb/Application.java
@@ -0,0 +1,14 @@
+package io.dev.deneb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@
+public class CloudServerConfigDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CloudServerConfigDemoApplication.class, args);
+  }
+
+}
